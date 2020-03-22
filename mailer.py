456f09d82812223bfce3b03526a4bc929bfd6426
Arghyadeep Das <arghyadeep.d@somaiya.edu>
2020-03-22 09:58:59 +0530

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -1,10 +1,12 @@
 import smtplib, ssl,csv
+from getpass import getpass
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email = "abc@gmail.com"  # Enter your address
 #receiver_email = "xyz@gmail.com"  # Enter receiver address if 1-to-1 communication
-password = input("Type your password and press enter: ")
+password = getpass("Type your password and press enter: ")
+filename= input("Enter exact filename with extension of the CSV file: ") #make sure the file is in the same directory as this script
 message = """\
 Subject: Enter your subject here!
 
@@ -15,10 +17,10 @@ Hi {name},
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
 	server.login(sender_email, password)
-	with open("qwerty.csv") as mail_list: #This example is for CSV received from Google Form
+	with open(filename) as mail_list:
 		reader = csv.reader(mail_list)
 		next(reader)  # Skip header row
 		for L in reader:
-			print(f"Sending email to {L[2]}, email: {L[1]}")
-			server.sendmail(sender_email, L[1], message.format(name=L[2]))
+			print(f"Sending email to {L[2]}, email: {L[1]}") #values wrt a particular CSV
+			server.sendmail(sender_email, L[1], message.format(name=L[2].split()[0]))
 	
