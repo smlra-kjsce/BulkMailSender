8c5fb4812812451283ec77176ed932eace898aa3
arghyadeep99 <arghyadeep.d@somaiya.edu>
2021-09-04 13:20:13 +0530

    Updated with time pause to avoid connection refused error

--- mailer.py
+++ mailer.py
@@ -1,5 +1,6 @@
 import smtplib, ssl,csv
 from getpass import getpass
+from time import time
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
@@ -11,16 +12,28 @@ message = """\
 Subject: Enter your subject here!
 
 Hi {name},
-	This mail is sent using Python.
+    This mail is sent using Python.
 """
 
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-	server.login(sender_email, password)
-	with open(filename) as mail_list:
-		reader = csv.reader(mail_list)
-		next(reader)  # Skip header row
-		for L in reader:
-			print(f"Sending email to {L[2]}, email: {L[1]}") #values wrt a particular CSV
-			server.sendmail(sender_email, L[1], message.format(name=L[2].split()[0]))
-	
+    server.login(sender_email, password)
+    with open(filename) as mail_list:
+        reader = csv.reader(mail_list)
+
+        next(reader)  # Skip header row
+
+        #If you want to resume sending from row X of CSV, due to SMTPConnection Refusal, 
+        #uncomment the lines below:
+
+        #for i in range(X-1): # -1 compensates for the header that has been skipped
+        #	next(reader)
+        
+
+        for i,L in enumerate(reader):
+            if i%50 == 0:
+                time.sleep(2.5) #pause for 2.5 seconds to avoid connection refused error
+
+            print(f"Sending email to {L[2]}, email: {L[1]}") #values wrt a particular CSV
+            server.sendmail(sender_email, L[1], message.format(name=L[2].split()[0]))
+            time.sleep(0.25) #pause for 0.25 seconds to avoid connection refused error
